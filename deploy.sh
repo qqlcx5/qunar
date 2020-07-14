e809ee3e36692093f6fa654e5eb1a7de7bb58bb4
qqlcx5 <893917884@qq.com>
2020-07-14 22:16:07 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 # 确保脚本抛出遇到的错误
 set -e
-npm run docs:build && cd dist
+npm run build && cd dist
 # github && 码云
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/qqlcx5/qunar.git gh-pages
+git push -f git@github.com:qqlcx5/qunar.git master:gh-pages
 cd -
